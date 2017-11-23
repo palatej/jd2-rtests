e7efe3e8ef3593153c8f6feada40e4fb1cc5607b
Jean Palate <jean.palate@nbb.be>
2017-11-23 18:21:32 +0100

    X11 (complete) and new implementation of tdvar

--- R files/jd2_x11.R
+++ R files/jd2_x11.R
@@ -13,17 +13,44 @@ setClass(
   representation = representation(spec="jobjRef")
 )
 
-jd2_x11<-function(y, mode="Multiplicative", nfcasts=0, nbcasts=0){
-  if (!is.ts(y)){
-    stop("y must be a time series")
+jd2_x11<-function(series, mode="Multiplicative", 
+                  trendma=0, 
+                  seasonalma=c("msr"),
+                  sigmalim=c(1.5, 2.5), 
+                  nfcasts=0, 
+                  nbcasts=0, 
+                  seasonal=TRUE){
+  if (!is.ts(series)){
+    stop("series must be a time series")
   }
   
   jspec=.jnew("ec/satoolkit/x11/X11Specification")
   jmode<-.jcall("ec/satoolkit/DecompositionMode", "Lec/satoolkit/DecompositionMode;", "valueOf", mode)
   .jcall(jspec, "V", "setMode", jmode)
+  # Should be checked: lower>1, upper > lower
+  .jcall(jspec, "V", "setLowerSigma", sigmalim[1])
+  .jcall(jspec, "V", "setUpperSigma", sigmalim[2])
   .jcall(jspec, "V", "setForecastHorizon", as.integer(nfcasts))
   .jcall(jspec, "V", "setBackcastHorizon", as.integer(nbcasts))
-  jrslt<-.jcall("ec/tstoolkit/jdr/x11/X11Monitor", "Lec/tstoolkit/jdr/x11/X11Monitor$Results;", "process", ts_r2jd(y), jspec)
+  .jcall(jspec, "V", "setSeasonal", seasonal)
+  .jcall(jspec, "V", "setHendersonFilterLength", as.integer((trendma)))
+  # seasonal filters
+  freq=frequency(series)
+  if (length(seasonalma)==1){
+    jd_seas<-.jcall("ec/satoolkit/x11/SeasonalFilterOption", "Lec/satoolkit/x11/SeasonalFilterOption;", "valueOf", seasonalma[1])
+    .jcall(jspec, "V", "setSeasonalFilter", jd_seas)
+  }else if (length(seasonalma) == freq){
+    jd_allseas=list()
+    for (i in 1:freq){
+      jd_seas<-.jcall("ec/satoolkit/x11/SeasonalFilterOption", "Lec/satoolkit/x11/SeasonalFilterOption;", "valueOf", seasonalma[i])
+      jd_allseas[[i]]<-jd_seas
+    }
+    .jcall(jspec, "V", "setSeasonalFilters", .jarray(jd_allseas, contents.class ="ec/satoolkit/x11/SeasonalFilterOption" ))
+  }else{
+    stop("Invalid seasonalma option")
+  }
+  
+  jrslt<-.jcall("ec/tstoolkit/jdr/x11/X11Monitor", "Lec/tstoolkit/jdr/x11/X11Monitor$Results;", "process", ts_r2jd(series), jspec)
   new (Class = "JD2_X11", internal = jrslt, spec=jspec)
 }
 
@@ -31,12 +58,12 @@ setMethod("saDecomposition", "JD2_X11", function(object){
   if (is.jnull(object@internal)){
     return (NULL)
   }else{
-    y<-proc_ts(object@internal, "decomposition.b1")
+    series<-proc_ts(object@internal, "decomposition.b1")
     sa<-proc_ts(object@internal, "decomposition.d11")
     trend<-proc_ts(object@internal, "decomposition.d12")
     seas<-proc_ts(object@internal, "decomposition.d10")
     irr<-proc_ts(object@internal, "decomposition.d13")
-    return (ts.union(y, sa, trend, seas, irr))    
+    return (ts.union(series, sa, trend, seas, irr))    
   }
 })
 
---
+++ R files/testtdvar.R
@@ -0,0 +1,33 @@
+source("./R files/jd2_init.R")
+source("./R files/jd2_tdvar.R")
+
+load("./Data/ABS.rda")
+
+tdvar_all<-function(s, log=TRUE, td="TD7", var="Default",aicdiff=0){
+  if (log){
+    s<-log(s)
+  }
+  x<-jd2_tdvar(s, td, var, aicdiff)
+  show(x)
+  c<-result(x, "coefficients.value")
+  ec<-result(x, "coefficients.stde")
+  w<-c[,dim(c)[2]]
+  ew<-ec[,dim(c)[2]]
+  
+  plot(w, type="l", col="red", ylim=c(min(w-ew), max(w+ew)))
+  lines(w+ew, col="gray")
+  lines(w-ew, col="gray")
+  
+  plot(c[,1], type="l", ylim=c(min(c), max(c)))
+  clrs<-c( "black","red", "green3","blue","cyan","magenta","gray")
+  for (i in 2: dim(c)[2]){
+    lines(c[,i],col=clrs[i])
+  }
+}
+
+tdvar_all(ABS$X0.2.20.10.M)
+tdvar_all(ABS$X0.2.20.10.M, var="Contrasts")
+
+
+
+
