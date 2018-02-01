f8051b50a39347a88318a6fc865a3140fc56489b
Jean Palate <jean.palate@nbb.be>
2018-02-01 11:26:54 +0100

    New X13 spec

--- R files/jd2_regarima.R
+++ R files/jd2_regarima.R
@@ -24,7 +24,9 @@ jd2_regarima<-function(series, spec=c("RG5c", "RG0", "RG1", "RG2c", "RG3", "RG4c
     stop("series must be a time series")
   }
   spec<-match.arg(spec)
-  jspec<-.jcall("ec/tstoolkit/modelling/arima/x13/RegArimaSpecification","Lec/tstoolkit/modelling/arima/x13/RegArimaSpecification;", "fromString", spec)
+  jrspec<-.jcall("jdr/spec/x13/RegArimaSpec", "Ljdr/spec/x13/RegArimaSpec;", "of", spec)
+  jspec<-.jcall(jrspec, "Lec/tstoolkit/modelling/arima/x13/RegArimaSpecification;", "getCore")
+  #jspec<-.jcall("ec/tstoolkit/modelling/arima/x13/RegArimaSpecification","Lec/tstoolkit/modelling/arima/x13/RegArimaSpecification;", "fromString", spec)
   
   jrslt<-.jcall("ec/tstoolkit/jdr/regarima/Processor", "Lec/tstoolkit/jdr/regarima/Processor$Results;", "x12", ts_r2jd(series), jspec)
   new (Class = "JD2_RegArima", internal = jrslt)
