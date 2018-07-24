39698ba5275a4ad89fbf4eb384daa358afae2e75
palatej <jean.palate@nbb.be>
2018-07-24 16:20:38 +0200

    MTD. Corrections

--- R files/jd2_mtd.R
+++ R files/jd2_mtd.R
@@ -14,7 +14,17 @@ jd2_mtd<-function(s, windowLength=11, smoothingLength=5, preprocessor="tramo", o
   jd_s<-ts_r2jd(s)
   jrslt<-.jcall("ec/tstoolkit/jdr/tdvar/MovingTradingDays", 
                 "Lec/tstoolkit/jdr/tdvar/MovingTradingDays$Results;", 
-                "process", jd_s, as.integer(windowLength), as.integer(smoothingLength), preprocessor, option, reestimate)
+                "movingWindow", jd_s, as.integer(windowLength), as.integer(smoothingLength), preprocessor, option, reestimate)
+  new (Class = "JD2_MovingTradingDays", internal = jrslt)
+  
+}
+
+jd2_tvtd<-function(s, contrasts=FALSE, preprocessor="tramo", option="TR5a"){
+  
+  jd_s<-ts_r2jd(s)
+  jrslt<-.jcall("ec/tstoolkit/jdr/tdvar/MovingTradingDays", 
+                "Lec/tstoolkit/jdr/tdvar/MovingTradingDays$Results2;", 
+                "timeVarying", jd_s, contrasts, preprocessor, option)
   new (Class = "JD2_MovingTradingDays", internal = jrslt)
   
 }
--- R files/testtdvar.R
+++ R files/testtdvar.R
@@ -1,10 +1,13 @@
 source("./R files/jd2_init.R")
 source("./R files/jd2_tdvar.R")
 source("./R files/jd2_mtd.R")
+source("./R files/jd2_x13.R")
+source("./R files/jd2_tests.R")
 
 load("./Data/ABS.rda")
 
 tdvar_all<-function(s, log=TRUE, td="TD7", var="Default",aicdiff=0){
+  orig<-s
   if (log){
     s<-log(s)
   }
@@ -25,7 +28,7 @@ tdvar_all<-function(s, log=TRUE, td="TD7", var="Default",aicdiff=0){
     lines(c[,i],col=clrs[i])
   }
 
-  xx<-jd2_mtd(s)
+  xx<-jd2_mtd(orig, windowLength = 7,reestimate = TRUE)
   cc<-result(xx, "mtd.smoothedcoefficients")
   s<--rowSums(cc)
   cc<-cbind(cc,s)
@@ -34,11 +37,39 @@ tdvar_all<-function(s, log=TRUE, td="TD7", var="Default",aicdiff=0){
   for (i in 2: dim(cc)[2]){
     lines(cc[,i],col=clrs[i])
   }
+
+#time varying trading days
+  a<-jd2_tvtd(orig)
+#available results
+#dictionary(a)
+#take the calendar effects and the final sa series
+  td_a<-result(a,"tvtd.tde")
+  sa_a<-result(a,"sa")
+
+# same with moving window
+  b<-jd2_mtd(orig)
+#available results
+#dictionary(b)
+#take the calendar effects and the final sa series
+  td_b<-result(b,"mtd.tde")
+  sa_b<-result(b,"sa")
+
+  ts.plot(ts.union(td_a, td_b), col=c("blue", "red"))
+  ts.plot(ts.union(sa_a, sa_b), col=c("blue", "red"))
+
+  sa<-result(jd2_x13(orig), "sa")
+  cat("\nTD tests on the last 15 years\n" )
+  cat("residual TD effects are significant if the PValue < 0.05 \n" )
+  cat("\nx13\n" )
+  print(jd2_td_FTest(sa, nyears=15)[2])
+  cat("\nTime Varying\n")
+  print(jd2_td_FTest(sa_a, nyears=15)[2])
+  cat("\nMoving window\n" )
+  print(jd2_td_FTest(sa_b, nyears=15)[2])
+  
 }
 
-tdvar_all(ABS$X0.2.20.10.M)
+tdvar_all(ABS$X0.2.09.10.M)
 #tdvar_all(ABS$X0.2.20.10.M, var="Contrasts")
-
-
-
+#tdvar_all(ABS$X0.2.41.10.M )
 
