7bd0767a83616206c832af1410c233a10225d28d
palatej <jean.palate@nbb.be>
2020-06-05 12:38:49 +0200

    v2.2.3RC2

--- R files/jd2_init.R
+++ R files/jd2_init.R
@@ -3,8 +3,8 @@ if(!require(rJava)){
 }
 library("rJava")
 .jinit()
-.jaddClassPath("./Java/demetra-tstoolkit-2.2.2-SNAPSHOT.jar")
-.jaddClassPath("./Java/jdr-2.2.2-SNAPSHOT.jar")
-.jaddClassPath("./Java/mtd-core-2.2.2-SNAPSHOT.jar")
+.jaddClassPath("./Java/demetra-tstoolkit-2.2.3-RC2.jar")
+.jaddClassPath("./Java/jdr-2.2.3-RC2.jar")
+.jaddClassPath("./Java/mtd-core-2.2.3-RC2.jar")
 
 jd_clobj<-.jcall("java/lang/Class", "Ljava/lang/Class;", "forName", "java.lang.Object")
